bf7720bb7f0992243b81f164070a327918a2d71a
Antoine Gardiol <antoine.gardiol@gmail.com>
2022-04-21 16:17:48 +0200

    feat: add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 import { MapComponent } from './components/map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ToolbarComponent } from './components/toolbar/toolbar.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgxMapLibreGLModule
+    NgxMapLibreGLModule,
+    BrowserAnimationsModule,
+
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
